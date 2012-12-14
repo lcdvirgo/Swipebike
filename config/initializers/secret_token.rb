6d3883ea03f08f4d73cc35a44c4fb70c72fc958c
acetang <acetang@uw.edu>
2012-12-13 21:50:36 -0800

    First commit of SwipeBike

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swipebike::Application.config.secret_token = '1330c38ecaad3166b344f4fdadfaf3bc953adca698bbaa61279c7d151b3f2ee531e1e8770669a8d6f952f497d90040924c84ca6af67275532e9814ccd8303f06'
